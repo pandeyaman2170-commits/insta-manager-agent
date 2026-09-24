71f2b7cfda980c28374447693afffc4ae7f33d0e
pandeyaman2170-commits <pandeyaman2170@gmail.com>
2026-09-24 09:21:49 +0530

    fix error

--- backend/src/main/java/com/clipgrowth/clipgrowth_backend/processing/VideoProcessor.java
+++ backend/src/main/java/com/clipgrowth/clipgrowth_backend/processing/VideoProcessor.java
@@ -1,38 +1,8 @@
 package com.clipgrowth.clipgrowth_backend.processing;
 
-import com.clipgrowth.clipgrowth_backend.analysis.AnalysisStatus;
-import com.clipgrowth.clipgrowth_backend.analysis.VideoAnalysis;
-import com.clipgrowth.clipgrowth_backend.analysis.VideoAnalysisRepository;
 import com.clipgrowth.clipgrowth_backend.video.Video;
-import org.springframework.stereotype.Service;
 
-@Service
-public class LocalVideoProcessor implements VideoProcessor {
+public interface VideoProcessor {
 
-    private final VideoAnalysisRepository videoAnalysisRepository;
-
-    public LocalVideoProcessor(VideoAnalysisRepository videoAnalysisRepository) {
-        this.videoAnalysisRepository = videoAnalysisRepository;
-    }
-
-    @Override
-    public void process(Video video, ProcessingJob job) {
-
-        job.setStatus(ProcessingJobStatus.PROCESSING);
-
-        VideoAnalysis analysis = new VideoAnalysis();
-        analysis.setVideo(video);
-        analysis.setStatus(AnalysisStatus.PROCESSING);
-
-        videoAnalysisRepository.save(analysis);
-
-        System.out.println("Processing video: " + video.getId());
-
-        // Real transcription/video analysis will be added later.
-
-        analysis.setStatus(AnalysisStatus.COMPLETED);
-        videoAnalysisRepository.save(analysis);
-
-        job.setStatus(ProcessingJobStatus.COMPLETED);
-    }
+    void process(Video video, ProcessingJob job);
 }
